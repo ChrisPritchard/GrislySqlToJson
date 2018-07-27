6cff9ab0909c95b823708f0353663b893f9ee689
Christopher <chrispritchard.nz@gmail.com>
2018-07-28 11:44:02 +1200

    handles comments and puts files in /out

--- Data/Comment.cs
+++ Data/Comment.cs
@@ -1,14 +1,16 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Newtonsoft.Json;
 
 namespace GrislyGrotto
 {
     public class Comment
     {
         public int Id { get; set; }
+        [JsonIgnore]
         public string Post_Key {get;set;}
-        [ForeignKey("Post_Key")]
+        [ForeignKey("Post_Key"), JsonIgnore]
         public virtual Post Post { get; set; }
 
         [Required]
--- Program.cs
+++ Program.cs
@@ -14,9 +14,14 @@ namespace GrislySqlToJson
             var connString = args[0];
             var context = new GrislyGrottoDbContext(connString);
 
-            var posts = context.Posts.Take(3).ToArray();
+            var outDir = "./out";
+            if(Directory.Exists(outDir))
+                Directory.Delete(outDir, true);
+            Directory.CreateDirectory(outDir);
+
+            var posts = context.Posts.Include(o => o.Comments).Take(100).ToArray();
             foreach(var post in posts)
-                File.WriteAllText(post.Key + ".json", JsonConvert.SerializeObject(post));
+                File.WriteAllText($"{outDir}/{post.Key}.json", JsonConvert.SerializeObject(post));
         }
     }
 }
