12e712583900a6c742530a2358b353ac688b34ac
Christopher <chrispritchard.nz@gmail.com>
2018-07-28 12:02:34 +1200

    Now also writes a ordered index file

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using GrislyGrotto;
 using System.IO;
@@ -19,12 +20,22 @@ namespace GrislySqlToJson
             using(var zipFile = new FileStream($"./allposts-{DateTime.Now.ToString("dd-MM-yyyy")}.zip", FileMode.Create))
             using(var archive = new ZipArchive(zipFile, ZipArchiveMode.Create))
             {
+                var index = new List<(string,string)>();
                 foreach(var post in posts)
                 {
                     var entry = archive.CreateEntry(post.Key + ".json");
                     using(var writer = new StreamWriter(entry.Open()))
                         writer.Write(JsonConvert.SerializeObject(post));
+
+                    index.Add((post.Date.ToString("s"), post.Key));
                 }
+
+                var indexEntry = archive.CreateEntry("index.txt");
+                using(var writer = new StreamWriter(indexEntry.Open()))
+                    index.OrderByDescending(o => o).ToList().ForEach(o => {
+                        var (date, key) = o;
+                        writer.WriteLine(date + "\t" + key);
+                    });
             }
         }
     }
